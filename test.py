5bdc31e471fb3f2ce416fb0c573baadb4317940b
Isaiah P <124836428+w1ggle@users.noreply.github.com>
2023-11-26 17:29:26 -0500

    attempting to get selenium to work

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+
 # Using readlines() to grab all the lines in the text file
 file1 = open('CCclasses.txt', 'r') #txt file that has every class taken at CC on a separate line (grabbed from CC website)
 Lines = file1.readlines()
- 
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/99.0", 
+    "method": "GET"
+}
+
+NJtransferURL = 'https://njtransfer.org/artweb/listeqs.cgi?MI+NI'
+#page_to_scrape = requests.get(NJtransferURL,headers=headers) 
+#soup = BeautifulSoup(page_to_scrape.text, 'html.parser') 
+
+driver = webdriver.Chrome()
+driver.get(NJtransferURL)
+
+
+
 for line in Lines:
 
     #if (line.find("Elective") and line.find("OR") and line.find("Semester") == -1): #putting it into 1 line is not working as expected, going to use a lot of if statements
